a2538e64297d739fa2d6987961d790f269e1e721
yogurt_lei <yogurt_lei@foxmail.com>
2018-10-09 17:17:42 +0800

    小i新闻爬虫

---
+++ spider/XiaoI.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+# yogurt_lei@foxmail.com
+# 2018-10-09 11:37
+# 小i新闻爬虫
+
+
+import time, datetime
+import json
+import requests
+
+HEADERS = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36'
+}
+XIAOI_SAVE_ACTION = 'xxx/kbase-core/action/spider/ai-of-week!save.htm'
+BASE_URL = "https://www.xiaoi.com"
+# 行业分类
+APP = {
+    "xiaoi": ["小i新闻", "/api/api.aspx?order=1538984151&intcount=5&Cmd=getnew"]
+}
+
+TEMPLATE = '<div style="width: 100%; padding-top: 10px; padding-bottom: 20px;" class="col-xs-12 container">\
+                                <div>\
+                                    <div class="col-sm-4 col-xs-12">\
+                                        <div style="font-weight: 700; font-size: 1.2em">\
+                                            {{title}}\
+                                        </div>\
+                                    <div style="padding-bottom: 10px; padding-top: 10px">\
+                                        <img src="' + BASE_URL + '/Upload/{{pic}}" style="width: 100%" class="NewImg" />\
+                                    </div>\
+                                </div>\
+                                <div class="col-sm-8  col-xs-12 lineheight NewDetail" style="text-align: left">\
+                                    {{content}}\
+                                </div>\
+                            </div>'
+
+if __name__ == '__main__':
+    print('[%s] start grab data' % time.strftime('%Y-%m-%d %H:%M:%S'))
+    news_data = {}
+    counter = 0
+    try:
+        for (key, value) in APP.items():
+            cate_name = value[0]
+            url = BASE_URL + value[1]
+            respJson = requests.get(url, headers=HEADERS).json()
+            data_list = []
+            for data in respJson:
+                id = data['id']
+                # 数据接口地址
+                url = BASE_URL + '/api/api.aspx?id=' + str(id) + '&Cmd=get_newsDetail'
+                title = data['title']
+                create_time = int(data['create_time'])
+                # 获取今日数据
+                now = datetime.datetime.now()
+                startTime = now - datetime.timedelta(hours=now.hour, minutes=now.minute, seconds=now.second,
+                                                     microseconds=now.microsecond)
+                endTime = startTime + datetime.timedelta(hours=23, minutes=59, seconds=59)
+                if int(startTime.timestamp()) <= create_time <= int(endTime.timestamp()):
+                    continue
+
+                counter = counter + 1
+                respJson = requests.get(url, headers=HEADERS).json()
+
+                data_list.append({
+                    'id': id,
+                    'url': url,
+                    'title': title,
+                    'content': TEMPLATE.replace('{{title}}', respJson['title'])
+                        .replace('{{content}}', respJson['content'])
+                        .replace('{{pic}}', respJson['pic'])
+                })
+            news_data[key] = {'category': cate_name, 'list': data_list}
+        print('[%s] complete grab total %s data' % (time.strftime('%Y-%m-%d %H:%M:%S'), counter))
+        params = {'newsData': json.dumps(news_data)}
+        print(news_data)
+        respJson = requests.post(XIAOI_SAVE_ACTION, params, headers=HEADERS, timeout=20).json()
+        print('[%s] complete save action: %s ' % (time.strftime('%Y-%m-%d %H:%M:%S'), respJson))
+        print('=============================================================================')
+    except Exception as e:
+        print(str(e))
