bc96ba4b730cce6f50e4fa9c867c65ec2284c974
yogurt_lei <yogurt_lei@foxmail.com>
2018-11-07 15:58:32 +0800

    fix: 置顶新闻处理&新闻时间不判断

--- spider/XiaoI.py
+++ spider/XiaoI.py
@@ -16,15 +16,16 @@ HEADERS = {
 BASE_URL = "https://www.xiaoi.com"
 
 now = datetime.datetime.now()
-start_time = now - datetime.timedelta(hours=now.hour, minutes=now.minute, seconds=now.second, microseconds=now.microsecond)
-end_time = start_time + datetime.timedelta(hours=23, minutes=59, seconds=59)
+start_time = now - datetime.timedelta(hours=now.hour, minutes=now.minute, seconds=now.second,
+                                      microseconds=now.microsecond)
 start_time_stamp = int(start_time.timestamp())
-end_time_stamp = int(end_time.timestamp())
 
 # 行业分类
 APP = {
     "xiaoi": ["小i新闻", "/api/api.aspx?order=" + str(start_time_stamp) + "&intcount=5&Cmd=getnew"]
 }
+# 置顶新闻
+HOT_NEW = '/api/api.aspx?order=99999999999&intcount=1&Cmd=getnew'
 
 TEMPLATE = '<div style="width: 100%; padding-top: 10px; padding-bottom: 20px;" class="col-xs-12 container">\
                 <div>\
@@ -41,47 +42,48 @@ TEMPLATE = '<div style="width: 100%; padding-top: 10px; padding-bottom: 20px;" c
                 </div>\
             </div>'
 
+
+def sub_parse(url):
+    respJson = requests.get(url, headers=HEADERS).json()
+    tmp_list = []
+    for data in respJson:
+        # 数据接口地址
+        data_id = data['id']
+        title = data['title']
+        data_url = BASE_URL + '/api/api.aspx?id=' + str(data_id) + '&Cmd=get_newsDetail'
+        respJson = requests.get(data_url, headers=HEADERS).json()
+
+        # 填充模版 替换转义字符
+        content = TEMPLATE.replace('{{title}}', respJson['title']) \
+            .replace('{{content}}', respJson['content']) \
+            .replace('{{pic}}', respJson['pic']) \
+            .replace('&lt;', '<') \
+            .replace('&gt;', '>') \
+            .replace('<img src=\"/upload/', '<img src=\"' + BASE_URL + '/upload/')
+
+        tmp_list.append({
+            'id': data_id,
+            'url': data_url,
+            'title': title,
+            'content': content
+        })
+
+    return tmp_list
+
+
 if __name__ == '__main__':
     print('[%s] start grab xiaoi news data' % time.strftime('%Y-%m-%d %H:%M:%S'))
     news_data = {}
-    counter = 0
     try:
-        for (key, value) in APP.items():
-            cate_name = value[0]
-            url = BASE_URL + value[1]
-            respJson = requests.get(url, headers=HEADERS).json()
-            data_list = []
-            for data in respJson:
-                id = data['id']
-                # 数据接口地址
-                url = BASE_URL + '/api/api.aspx?id=' + str(id) + '&Cmd=get_newsDetail'
-                title = data['title']
-                create_time = int(data['create_time'])
-                # 获取今日数据
-                if not start_time_stamp <= create_time <= end_time_stamp:
-                    continue
+        xiaoi = APP.get('xiaoi')
+        cate_name = xiaoi[0]
+        data_list = sub_parse(BASE_URL + HOT_NEW)
+        data_list.extend(sub_parse(BASE_URL + xiaoi[1]))
+        news_data['xiaoi'] = {'category': cate_name, 'list': data_list}
 
-                counter = counter + 1
-                respJson = requests.get(url, headers=HEADERS).json()
-
-                # 填充模版 替换转义字符
-                content = TEMPLATE.replace('{{title}}', respJson['title']) \
-                    .replace('{{content}}', respJson['content']) \
-                    .replace('{{pic}}', respJson['pic']) \
-                    .replace('&lt;', '<') \
-                    .replace('&gt;', '>') \
-                    .replace('<img src=\"/upload/', '<img src=\"' + BASE_URL + '/upload/')
-
-                data_list.append({
-                    'id': id,
-                    'url': url,
-                    'title': title,
-                    'content': content
-                })
-            news_data[key] = {'category': cate_name, 'list': data_list}
-        print('[%s] complete grab total %s data' % (time.strftime('%Y-%m-%d %H:%M:%S'), counter))
+        print('[%s] complete grab total %s data' % (time.strftime('%Y-%m-%d %H:%M:%S'), len(data_list)))
         params = {'newsData': json.dumps({'origin': '小i新闻', 'data': news_data})}
-        resp = requests.post('http://localhost:8081/kbase-core/action/spider/ai-of-day!save.htm',
+        resp = requests.post('http://xxxx/kbase-core/action/spider/ai-of-day!save.htm',
                              params, headers=HEADERS, timeout=120)
         if resp.status_code == 200 and resp.content:
             print('[%s] complete save action: %s ' % (time.strftime('%Y-%m-%d %H:%M:%S'), resp.json()))
