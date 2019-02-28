b873e69dc2540e5bede49c3a41ef2870fca509a0
yogurt_lei <yogurt_lei@foxmail.com>
2019-02-28 15:54:47 +0800

    feat: easy to use solr

---
+++ easy2solr/README.md
@@ -0,0 +1,16 @@
+
+
+> **easy to use solr7**
+
+![license](https://img.shields.io/github/license/mashape/apistatus.svg)
+
+> some easy to use solr7 script
+
+##### manual
+> **startup_solr.bat** : easy to start solr, you can edit your customize configuration.
+and it will use tail command to rolling refresh the logFile to show
+
+> **shutdown_solr.bat** : easy to stop **all** solr node
+
+> **tail.exe** : one implement tail command in windows platform (put it in your windows/system32 to use it)
+
---
+++ easy2solr/shutdown_solr.bat
@@ -0,0 +1,34 @@
+@echo off
+if "%OS%" == "Windows_NT" setlocal
+rem ---------------------------------------------------------------------------
+rem  easy stop solr7 by yogurt_lei
+rem ---------------------------------------------------------------------------
+
+set "CMD_LINE_ARGS=stop -all"
+set CURRENT_DIR=%cd%
+
+if not "%SOLR7_HOME%" == "" goto gotHome
+set SOLR7_HOME=%CURRENT_DIR%
+if exist "%SOLR7_HOME%\bin\solr.cmd" goto okHome
+cd ..
+set SOLR7_HOME=%cd%
+cd %CURRENT_DIR%
+
+:gotHome
+if exist "%SOLR7_HOME%\bin\solr.cmd" goto okHome
+echo The SOLR7_HOME environment variable is not defined correctly
+goto end
+:okHome
+
+set EXECUTABLE=%SOLR7_HOME%\bin\solr
+
+rem Check that target executable exists
+if exist "%EXECUTABLE%" goto okExec
+echo Cannot find %EXECUTABLE%
+echo This file is needed to run this program
+goto end
+:okExec
+
+call %EXECUTABLE% %CMD_LINE_ARGS%
+
+:end
