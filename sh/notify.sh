32f5cd5fc29a73fec4a462b38d2a952e7bc5a630
Yogurt lei <yogurt_lei@foxmail.com>
2021-09-03 14:43:50 +0800

    Update notify.sh
    remove key

--- sh/notify.sh
+++ sh/notify.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-curl 'https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=bbc0db25-53b3-4a40-b2cb-0c3e1abfdef9' \
+curl 'https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=' \
    -H 'Content-Type: application/json' \
    -d '
     {
       "msgtype": "markdown",
       "markdown": {
-          "content": "[掌上智库](http://git.qianet.cn/shwise_dev/wise-live/pipelines) <font color=\"warning\">构建成功,开发环境已更新.</font> 请相关同事注意 "
+          "content": "[x](y) <font color=\"warning\">构建成功,开发环境已更新.</font> 请相关同事注意 "
       }
     }'
